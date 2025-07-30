fed3c26663a68a920f4c4f3500f080efd9cc5292
julienverliac <julienverliacpro@gmail.com>
2025-07-30 23:53:52 +0200

    Animation and blinking on linear search

--- static/visualize_linearsearch.js
+++ static/visualize_linearsearch.js
@@ -4,8 +4,19 @@ function animateSteps(target) {
     const boxes = document.querySelectorAll('.array-box');
     let index = 0
 
+    const stepDuration = 1500; // Time for each step in milliseconds
+    const blinkCycle = 0.8 * 1000; // 0.8s chosen in CSS file
+    const blinkCount = 3;
+    const blinkDuration = blinkCycle * blinkCount;
+
+    // Start the animation with stepDuration as timing among each boxes
     function highlightNext(){
-        if (index >= boxes.length) return;
+        if (index >= boxes.length) {
+            setTimeout(() => {
+                resetAnimation();
+            }, stepDuration);
+            return;
+        }
 
         const box = boxes[index];
         const value = parseInt(box.textContent);
@@ -13,16 +24,33 @@ function animateSteps(target) {
         box.classList.add('active');
 
         setTimeout(() => {
+            box.classList.remove('active')
+
             if (value === target) {
-                box.classList.remove('active');
                 box.classList.add('found');
+                
+                setTimeout(() => {
+                    resetAnimation();
+                }, stepDuration);
             } else {
-                box.classList.remove('active');
                 box.classList.add('failed');
                 index++;
                 highlightNext();
             }
-        }, 1500);
+        }, stepDuration);
+    }
+
+    function resetAnimation(){
+        boxes.forEach(box => {
+            box.classList.remove('active', 'found', 'failed');
+            box.classList.add('blink');
+        });
+
+        setTimeout(() => {
+            boxes.forEach(box => box.classList.remove('blink'));
+            index = 0;
+            highlightNext();
+        }, blinkDuration);
     }
 
     highlightNext();
